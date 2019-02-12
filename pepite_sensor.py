063abf37ce0744312fef73b3446ac50a7b4b01c4
Kranelox <kranelox@gmail.com>
2019-02-12 12:22:37 -0300

    modificación para la función del sensor con return

--- pepite_sensor.py
+++ pepite_sensor.py
@@ -4,21 +4,21 @@ import RPi.GPIO as gpio
 
 gpio.setmode(gpio.BCM)
 
-def DHT11_data():
+def sens_temp_hum():
     humi, temp = dht.read_retry(dht.DHT11, 4)
-    return humi, temp
+    return temp, humi
 
-while True:
-    try:
-        humedad, temperatura = DHT11_data()
-
-        if humedad is not None and temperatura is not None:
-            print('Temp={0:0.1f}*C Humidity = {1:0.1f}%'.format(temperatura, humedad))
-        else:
-            print("Error en la lectura del sensor")
-
-        sleep(20)
-    except KeyboardInterrupt:
-        gpio.cleanup()
-        print("Programa Terminado")
-        break
+# while True:
+#     try:
+#         humedad, temperatura = DHT11_data()
+#
+#         if humedad is not None and temperatura is not None:
+#             print('Temp={0:0.1f}*C Humidity = {1:0.1f}%'.format(temperatura, humedad))
+#         else:
+#             print("Error en la lectura del sensor")
+#
+#         sleep(20)
+#     except KeyboardInterrupt:
+#         gpio.cleanup()
+#         print("Programa Terminado")
+#         break
