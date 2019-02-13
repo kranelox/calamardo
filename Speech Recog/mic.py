e0cb3674de44e73ce778a86d0d0dc04761d94e51
Kranelox <kranelox@gmail.com>
2019-02-13 16:32:29 -0300

    Correccion de variables entre app y js

--- Speech Recog/mic.py
+++ Speech Recog/mic.py
@@ -3,12 +3,13 @@ r = sr.Recognizer()
 mic = sr.Microphone()
 
 with mic as source:
+    r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
 
-BING_KEY = "f578f72c36634748a629df0301d167dd"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+# BING_KEY = "f578f72c36634748a629df0301d167dd"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+# try:
+#     print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
+# except sr.UnknownValueError:
+#     print("Microsoft Bing Voice Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
--- Speech Recog/voicerecog2.py
+++ Speech Recog/voicerecog2.py
@@ -10,15 +10,17 @@ with mic as source:
     audio = r.listen(source)
 
 you_said2 = r.recognize_google(audio_data=audio, language="es")
-you_said2 = you_said2.lower() #
+you_said2 = you_said2.lower() #convierte el texto todo a lowercase
 print(you_said2)
 
 if "alex" in you_said2:
     print("entré en Alexe")
     if "encende" in you_said2 and "luces" in you_said2:
         print("entré en encender luz")
+        requests.get("http://localhost:5000/lucesOn")
     elif "apagar" in you_said2 and "luces" in you_said2:
         print("entré en apagar luz")
+        requests.get("http://localhost:5000/lucesOff")
     elif "encende" in you_said2 and "ventilador" in you_said2:
         print("entré en encender ventilador")
     elif "apagar" in you_said2 and "ventilador" in you_said2:
