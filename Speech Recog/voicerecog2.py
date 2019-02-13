a6427c03f8527129589e408c4eb8b061649cf119
Kranelox <kranelox@gmail.com>
2019-02-13 17:18:18 -0300

    agregado el while:True y server para conexión remota

--- Speech Recog/voicerecog2.py
+++ Speech Recog/voicerecog2.py
@@ -3,29 +3,31 @@ import requests
 
 r = sr.Recognizer()
 mic = sr.Microphone()
+SERVER = "http://192.168.1.207:5000"
 
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    print("habla perra")
-    audio = r.listen(source)
+while True:
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        print("habla perra")
+        audio = r.listen(source)
 
-you_said2 = r.recognize_google(audio_data=audio, language="es")
-you_said2 = you_said2.lower() #convierte el texto todo a lowercase
-print(you_said2)
+    you_said2 = r.recognize_google(audio_data=audio, language="es")
+    you_said2 = you_said2.lower() #convierte el texto todo a lowercase
+    print(you_said2)
 
-if "alex" in you_said2:
-    print("entré en Alexe")
-    if "encende" in you_said2 and "luces" in you_said2:
-        print("entré en encender luz")
-        requests.get("http://localhost:5000/lucesOn")
-    elif "apagar" in you_said2 and "luces" in you_said2:
-        print("entré en apagar luz")
-        requests.get("http://localhost:5000/lucesOff")
-    elif "encende" in you_said2 and "ventilador" in you_said2:
-        print("entré en encender ventilador")
-        requests.get("http://localhost:5000/ventiOn")
-    elif "apagar" in you_said2 and "ventilador" in you_said2:
-        print("entré en apagar ventilador")
-        requests.get("http://localhost:5000/ventiOff")
-    else:
-        print("habla bien carajo!")
+    if "alex" in you_said2 or "pepit" in you_said2:
+        print("entré en Alexe")
+        if "encende" in you_said2 and "luces" in you_said2:
+            print("entré en encender luz")
+            requests.get(SERVER + "/lucesOn")
+        elif "apagar" in you_said2 and "luces" in you_said2:
+            print("entré en apagar luz")
+            requests.get(SERVER + "/lucesOff")
+        elif "encende" in you_said2 and "ventilador" in you_said2:
+            print("entré en encender ventilador")
+            requests.get(SERVER + "/ventiOn")
+        elif "apagar" in you_said2 and "ventilador" in you_said2:
+            print("entré en apagar ventilador")
+            requests.get(SERVER + "/ventiOff")
+        else:
+            print("habla bien carajo!")
